9b61014f127385bd37caa82aece2ae358426e3b2
Vijay Baclay <vijay.baclay@proweaver.net>
2024-10-02 13:05:57 +0800

    finished responsive

--- js/script.js
+++ js/script.js
@@ -45,3 +45,46 @@ document.addEventListener('click', function(event) {
     }
 });
 
+// Burger
+document.getElementById('burger').addEventListener('click', function() {
+    const navMenu = document.getElementById('navMenu');
+    const burger = document.getElementById('burger');
+
+   
+    if (window.innerWidth <= 1024) {
+        navMenu.classList.toggle('nav-open');
+        navMenu.classList.toggle('nav-closed');
+        
+       
+        burger.classList.toggle('active');
+    }
+});
+
+// Close button functionality to close the nav menu
+document.getElementById('closeNav').addEventListener('click', function() {
+    const navMenu = document.getElementById('navMenu');
+    const burger = document.getElementById('burger');
+
+    if (window.innerWidth <= 1024) {
+        navMenu.classList.add('nav-closed');
+        navMenu.classList.remove('nav-open');
+
+        burger.classList.remove('active');
+    }
+});
+
+
+document.addEventListener('click', function(event) {
+    const navMenu = document.getElementById('navMenu');
+    const burger = document.getElementById('burger');
+
+
+    if (window.innerWidth <= 1024 && navMenu.classList.contains('nav-open')) {
+        if (!navMenu.contains(event.target) && !burger.contains(event.target)) {
+            navMenu.classList.add('nav-closed');
+            navMenu.classList.remove('nav-open');
+            
+            burger.classList.remove('active');
+        }
+    }
+});
